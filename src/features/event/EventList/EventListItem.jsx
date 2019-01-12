87f249215003c154e352bdfabd0d580f7840252b
MaxYuan <maxyuan.yuan@gmail.com>
2019-01-12 11:36:49 +1100

    static view finish

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,45 +1,44 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-import EventAttendee from './EventAttendee'
+import EventAttendee from "./EventAttendee";
 
 class EventListItem extends Component {
   render() {
+    const {event} = this.props;
     return (
-      <div>
-        <Segment.Group>
-          <Segment>
-            <Item.Group>
-              <Item>
-                <Item.Image size="tiny" circular src="https://randomuser.me/api/portraits/women/42.jpg" />
-                <Item.Content>
-                  <Item.Header as="a">Event Title</Item.Header>
-                  <Item.Description>
-                    Hosted by <a>hosted by</a>
-                  </Item.Description>
-                </Item.Content>
-              </Item>
-            </Item.Group>
-          </Segment>
-          <Segment>
-            <span>
-              <Icon name="clock" /> date |
-              <Icon name="marker" /> time
-            </span>
-          </Segment>
-          <Segment secondary>
-            <List horizontal>
-              <EventAttendee/>
-              <EventAttendee/>
-              <EventAttendee/>
-            </List>
-          </Segment>
-          <Segment clearing>
-            <span>Description will go here</span>
-            <Button as="a" color="teal" floated="right" content="View" />
-          </Segment>
-        </Segment.Group>
-      </div>
+      <Segment.Group>
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+              <Item.Content>
+                <Item.Header as="a">Event Title</Item.Header>
+                <Item.Description>
+                  Hosted by <a>{event.hostedBy}</a>
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+        <Segment>
+          <span>
+            <Icon name="clock" /> {event.date} |
+            <Icon name="marker" /> {event.venue}
+          </span>
+        </Segment>
+        <Segment secondary>
+          <List horizontal>
+          {event.attendees.map((attendee)=>
+            <EventAttendee key={attendee.id} attendee={attendee}/>
+            )}
+          </List>
+        </Segment>
+        <Segment clearing>
+          <span>{event.description}</span>
+          <Button as="a" color="teal" floated="right" content="View" />
+        </Segment>
+      </Segment.Group>
     );
   }
 }
-export default EventListItem
+export default EventListItem;
